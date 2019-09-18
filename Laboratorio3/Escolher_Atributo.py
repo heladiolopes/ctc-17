e6e988184e3c6cf63ed0d2894eb0db0dbbb6c52d
beethovenbrandaofilho <beethovenbrandaofilho@gmail.com>
2019-09-18 13:37:44 -0300

    Ajustando cálculo de ganhos (entropia)

--- Laboratorio3/Escolher_Atributo.py
+++ Laboratorio3/Escolher_Atributo.py
@@ -1,15 +1,16 @@
-from math import log
+from math import log, inf
 from collections import defaultdict
+from Leitura import Atributos_Exemplos
 
 # atributos -> dicionário: chave é o índice de exemplos e o valor corresponde a uma lista de valores possíveis
 # exemplos -> matriz: cada linha corresponde a um exemplo (com atributos e padrão)
 
 
-def examples_with_attribute(examples, attribute_value):
+def examples_with_attribute(examples, attribute, attribute_value):
     new_examples = []
 
     for i in range(len(examples)):
-        if attribute_value in examples[i]:
+        if attribute_value == examples[i][attribute]:
             new_examples.append(examples[i])
 
     return new_examples
@@ -28,6 +29,9 @@ def initial_entropy(examples):
     for attribute_value in freq:
         entropy += (-freq[attribute_value])*(log(freq[attribute_value]/sum_freq, 2))/sum_freq
 
+    #print(freq)
+    #print(entropy)
+
     return entropy
 
 
@@ -35,24 +39,25 @@ def choose_attribute(attributes, examples):
     examples_size = len(examples)
     e = initial_entropy(examples)
 
-    best_gain = -1
+    best_gain = -inf
     best_gain_index = -1
     for attribute in attributes:
         gain = e
         for attribute_value in attributes[attribute]:
-            new_examples = examples_with_attribute(examples, attribute_value)
+            new_examples = examples_with_attribute(examples, attribute, attribute_value)
             attribute_value_size = len(new_examples)
             attribute_value_entropy = initial_entropy(new_examples)
             gain -= (float(attribute_value_size)/float(examples_size))*attribute_value_entropy
         if gain > best_gain:
             best_gain = gain
             best_gain_index = attribute
-        # print(attribute, gain)
+        #print(attribute, gain)
 
     return best_gain_index
 
 
 if __name__ == "__main__":
+    """
     attributes = {0: ["Ensolarado", "Nublado", "Chuvoso"], 1: ["Quente", "Boa", "Fria"],
                   2: ["Alta", "Normal"], 3: ["Forte", "Fraco"]}
     examples = [["Ensolarado", "Quente", "Alta", "Fraco", "NAO"],
@@ -69,6 +74,9 @@ if __name__ == "__main__":
                 ["Nublado", "Boa", "Alta", "Forte", "SIM"],
                 ["Nublado", "Quente", "Normal", "Fraco", "SIM"],
                 ["Chuvoso", "Boa", "Alta", "Forte", "NAO"]]
-    _ = choose_attribute(attributes, examples)
+    """
 
+    examples, attributes = Atributos_Exemplos()
+
+    _ = choose_attribute(attributes, examples)
 
